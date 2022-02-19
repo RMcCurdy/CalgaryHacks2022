4872c948f1bbab347cb28302c150aad0a0f64a50
Robert McCurdy <rwmccurdy@hotmail.com>
2022-02-19 15:25:24 -0700

    Maps gets lat and long

--- reactapp/src/components/routePlanner/RoutePlanner.js
+++ reactapp/src/components/routePlanner/RoutePlanner.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Maps from '../maps/Maps';
+
 const RoutePlanner = () => {
-    return <div className='page-container'>RoutePlanner</div>;
+    return (
+        <div className='page-container'>
+            <Maps />
+        </div>
+    );
 };
 
 export default RoutePlanner;
