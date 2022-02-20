aa98b37b267213f757e5bc423c269a7ce8e880b3
Robert McCurdy <rwmccurdy@hotmail.com>
2022-02-19 20:35:25 -0700

    Added Chris Doughnut

--- reactapp/src/components/calc/EnvCalculator.js
+++ reactapp/src/components/calc/EnvCalculator.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import MyDoughnut from './MyDoughnut';
 
 const EnvCalculator = () => {
-    return <div className='page-container'>EnvCalculator</div>;
+    return (
+        <div className='page-container'>
+            <div className='about-header-container'>
+                <h6 className='about-semi-header'>- how we can -</h6>
+                <h1 className='about-header'>CALCULATE</h1>
+            </div>
+            <MyDoughnut />
+        </div>
+    );
 };
 
 export default EnvCalculator;
--- reactapp/src/components/routePlanner/RoutePlanner.js
+++ reactapp/src/components/routePlanner/RoutePlanner.js
@@ -5,6 +5,10 @@ import Maps from '../maps/Maps';
 const RoutePlanner = () => {
     return (
         <div className='page-container'>
+            <div className='about-header-container'>
+                <h6 className='about-semi-header'>- save the environment with -</h6>
+                <h1 className='about-header'>ECOMAPS</h1>
+            </div>
             <Maps />
         </div>
     );
