673b26143595492c407bceb3dae9cf7203ee0aae
MarkusPistner <74573488+MarkusP1@users.noreply.github.com>
2022-02-20 10:36:38 -0700

    about text

--- reactapp/src/components/howitworks/HowItWorks.js
+++ reactapp/src/components/howitworks/HowItWorks.js
@@ -9,21 +9,35 @@ const HowItWorks = () => {
             </div>
             <div className='content-card-container'>
                 <div className='card-container'>
-                    <div className='content-header'>Our Mission</div>
+                    <div className='content-img'><img src="../../images/idea.png"></img></div>
+                    <div className='content-header'>Background and Research</div>
                     <div className='content-descriptor'>
-                        EcoMaps wants to give commutors the opportunity to make smart and eco-friendly decisions when it comes to transportation.
+                        EcoMaps uses readily available emissions and fuel consumption data from Natural Resources Canada to create accurate ratings based on vehicle type. 
+                        For simplicity, we've proken down the vehicles into three main catagories: cars, SUVs and trucks.
+                        Alongside this, transit data gathered from multiple cities is combined and used to get the average ridership numbers for buses. 
+                        This allows for accurate emissions values per rider, meaning a more accurate EcoScore. 
+                        For walking on foot, the EcoScore depriciates with distance and time. 
+                        Walking is emissions friendly, but long distances make it unrealistic, therefore resulting in a lower score.
                     </div>
                 </div>
                 <div className='card-container'>
-                    <div className='content-header'>Our Mission</div>
+                    <div className='content-img-reverse'><img src="../../images/api.png"></img></div>
+                    <div className='content-header-reverse'>APIs and Technical Analysis </div>
                     <div className='content-descriptor'>
-                        EcoMaps wants to give commutors the opportunity to make smart and eco-friendly decisions when it comes to transportation.
+                        EcoMaps uses multiple map and geocode APIs to recieve and calculate location and distance data.
+                        The Geocode API is used to get accurate Latitude and Longitude values from entered text strings. 
+                        This location data can then be passed to the Bing Maps API where we recieve the distance and time values for multiple modes of transportation.
+                        Google Maps API is used concurrently to provide the visual map and markers for displaying the origin and destination points.
                     </div>
                 </div>
                 <div className='card-container'>
+                     <div className='content-img'><img src="../../images/mission.png"></img></div>
                     <div className='content-header'>Our Mission</div>
                     <div className='content-descriptor'>
-                        EcoMaps wants to give commutors the opportunity to make smart and eco-friendly decisions when it comes to transportation.
+                        EcoMaps wants to give commutors the opportunity to make smart and eco-friendly decisions when it comes to transportation. 
+                        Providing the data in a simple but relevant way allows commutors and transit users to make more informed decisions about the way they travel. 
+                        We hope to expand our service to have a greater, greener impact on the daily life of everyone.
+                       
                     </div>
                 </div>
             </div>
